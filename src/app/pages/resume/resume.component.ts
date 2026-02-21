cd93ff38e9528958a52e6ccb2883ad682df0cb6c
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-22 03:44:00 +0530

    refactor: register Resumecompone in Resume page imports

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../component/navbar/navbar.component";
 import { FooterComponent } from "../../component/footer/footer.component";
+import { ResumecomponeComponent } from "../../component/resumecompone/resumecompone.component";
 
 @Component({
   selector: 'app-resume',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, ResumecomponeComponent],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.css'
 })
