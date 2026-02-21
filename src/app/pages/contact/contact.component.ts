2440a79e12198b838c16b1ff3593290b84e47c5c
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-22 03:40:52 +0530

    refactor: register Contactcompone in Contact page imports

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../component/navbar/navbar.component";
 import { FooterComponent } from "../../component/footer/footer.component";
+import { ContactcomponeComponent } from "../../component/contactcompone/contactcompone.component";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, ContactcomponeComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
