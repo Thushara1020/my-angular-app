3865212007b25820485d7865aa9c4c9a3e92a36e
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-21 23:12:36 +0530

    feat: initialize ContactcomponeComponent class

---
+++ src/app/component/contactcompone/contactcompone.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contactcompone',
+  imports: [],
+  templateUrl: './contactcompone.component.html',
+  styleUrl: './contactcompone.component.css'
+})
+export class ContactcomponeComponent {
+
+}
