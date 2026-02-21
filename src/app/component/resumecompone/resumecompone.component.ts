d689d03069f50d193ac9faaff187d98944dfe360
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-22 03:38:10 +0530

    feat: initialize ResumecomponeComponent class

---
+++ src/app/component/resumecompone/resumecompone.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resumecompone',
+  imports: [],
+  templateUrl: './resumecompone.component.html',
+  styleUrl: './resumecompone.component.css'
+})
+export class ResumecomponeComponent {
+
+}
