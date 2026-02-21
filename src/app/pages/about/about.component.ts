251d5b9e1d01ff5b64bbc3754555cf93ced5abd0
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-22 03:39:42 +0530

    refactor: register Aboutcomponent in About page imports

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../component/navbar/navbar.component";
 import { FooterComponent } from "../../component/footer/footer.component";
+import { AboutcomponentComponent } from "../../component/aboutcomponent/aboutcomponent.component";
 
 @Component({
   selector: 'app-about',
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, AboutcomponentComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
